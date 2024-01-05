a1df83ee7264ce10e124c096df857deeb7878c00
Jason Sherrick <jasonsherrick@gmail.com>
2024-01-05 14:13:19 -0600

    needs to extract index with one call to page.evaluate()

--- src/statutes.js
+++ src/statutes.js
@@ -3,7 +3,7 @@ import puppeteer, { ElementHandle, Page } from 'puppeteer';
 
 const BASE_URL = 'https://www.ilga.gov/legislation/ilcs/ilcs.asp';
 
-const TOPICS = [
+const SERIES_NAMES = [
     'GOVERNMENT',
     'EDUCATION',
     'REGULATION',
@@ -31,41 +31,53 @@ const browser = await puppeteer.launch();
 const page = await browser.newPage();
 await page.goto(BASE_URL);
 
-const chapterList = await page.$$eval('td ul > *', list => {
-    let chapters = {};
-    let series = '';
-    for (const el of list) {
-        const line = el.innerText;
-        if (!line) continue;
+// const quickTest = await page.$eval('td ul', el => el.innerText );
 
-        let number = '';
-        let name = '';
-        let url = '';
-        if (line.includes('CHAPTER')) {
-            // regex pattern to match any string of up to 3 digits
-            number = line.match(/\d{1,3}/)[0];
-            name = line.split(number)[1].trim();
-            url = el.querySelector('a').href;
-        }
-        else {
-            // check if el is a div without using querySelector
-            if (el.tagName === 'DIV' && el.className === 'black10bold') {
-                for ( let i = 0; i < TOPICS.length; i++) {
-                    if (line.includes(TOPICS[i])) {
-                        chapters[SERIES_NUMBERS[i]] = {
-                            name: TOPICS[i],
+// console.log(quickTest);
+
+try {
+    const chapterList = await page.$$('td ul'); 
+} catch(err) {
+}
+
+if (!chapterList) {
+    console.log('no chapter list');
+}
+else {
+        let ilcs = {};
+        let seriesNumber = '';
+        
+    
+            let chapterNumber = '';
+            let chapterName = '';
+            let chapterURL = '';
+            if (line.includes('CHAPTER')) {
+                for ( let i = 0; i < SERIES_NAMES.length; i++) {
+                    if (line.includes(SERIES_NAMES[i])) {
+                        seriesNumber = SERIES_NUMBERS[i];
+                        ilcs[seriesNumber] = {
+                            majorTopic: TOPICS[i],
                             chapters: {}
                         };
                     }
                 }
             }
-
+            else {
+               // regex pattern to match any string of up to 3 digits
+               chapterNumber = line.match(/\d{1,3}/)[0];
+               chapterName = line.split(chapterNumber)[1].trim();
+               chapterURL = el.querySelector('a').href;
+               ilcs[seriesNumber].chapters[chapterNumber] = {
+                   name: chapterName,
+                   url: chapterURL
+               };
+            }
         }
-    }
-},
-TOPICS,
-SERIES_NUMBERS);
-
-console.log(chapterList);
+        return JSON.stringify(ilcs);
+    },
+    SERIES_NAMES,
+    SERIES_NUMBERS);
+    console.log(newList);
+}
 
 browser.close();
