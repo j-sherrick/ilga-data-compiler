ed9786f9925d7eb5289c71a05e492ab6952e1781
Jason Sherrick <jasonsherrick@gmail.com>
2024-01-23 23:18:35 -0600

    populating act arrays

--- src/controllers/statutesController.js
+++ src/controllers/statutesController.js
@@ -3,17 +3,23 @@ import { Chapter, Act, Topic, Subtopic } from "../models/StatuteSchemas.js";
 import readline from "readline";
 import connectDB from "./connectDB.js";
 
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
-export async function run() {
-    const rl = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout
-    });
-    const ilcs = await initILCSCrawler();
-    const chapters = ilcs.chapters.map(chapter => new Chapter(chapter));
-    
+async function populateActArrays(chapters) {
+ for (const chapter of chapters) {
+     const acts = await ilcs.getActsFromChapter(chapter);
+     chapter.acts = acts;
+ }
 }
 
-export async function populateActs(chapters) {
-
-}
+export default {
+    async  run() {
+        const ilcs = await initILCSCrawler();
+        const chapters = ilcs.chapters.map(chapter => new Chapter(chapter));
+        console.log(`Found ${chapters.length} chapters`);
+        console.log(`First chapter: ${JSON.stringify(chapters[0])}`);
+    }    
+}    
