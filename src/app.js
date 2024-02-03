716ae99d44eaa8d2d21f8188b89d7b881899adcd
Jason Sherrick <jasonsherrick@gmail.com>
2024-02-03 17:57:30 -0600

    Refactor ILCSController and add new functions for initializing ILCSCollection, Acts, and Sections

--- src/app.js
+++ src/app.js
@@ -1,5 +1,17 @@
 import 'dotenv/config';
 
-import controller from './controllers/statutesController.js';
+import controller from './controllers/ILCSController.js';
+import { initILCSCrawler } from './services/ILCSCrawler.js';
 
-await controller.initILCSCollection();
+const { initILCSCollection, initActs, initSections, close } = controller;
+
+async function main() {
+    const { chapters, topics } = await initILCSCollection();
+    for (let chapter of chapters) {
+        const { acts, subtopics } = await initActs(chapter);
+        for (let act of acts) {
+            const sections = await initSections(act);
+        }
+    }
+    await close();
+}
