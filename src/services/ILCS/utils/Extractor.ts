48117d400f23820e9d5d016840de9ebac53c91e3
Jason Sherrick <jasonsherrick@gmail.com>
2024-03-18 11:46:05 -0500

    TypeScript refactor Extractor

--- src/services/ILCS/utils/Extractor.ts
+++ src/services/ILCS/utils/Extractor.ts
@@ -12,15 +12,21 @@ import { ENTIRE_ACT_LINK } from '../constants/strings.js';
  * This function is a pageFunction to be evaluated in the browser context by Puppeteer. It expects the result of a
  * querySelectorAll call on 'td ul *' selector, and returns a formatted string representing the index of ILCS chapters.
  * 
- * @param {Element[]} ulChildren children of a UL element that may contain a list of chapters or acts
- * @param {String} titleString string constant for formatting the title of a chapter or act
- * @param {String} urlString string constant for formatting the url to the page containing a chapter or act 
- * @param {String} topicString string constant for formatting the topic of a chapter or subtopic of an act.
- * @param {String} nlChar string constant for formatting a newline character
- * @returns {String} chapterIndexString - The formatted string representing a list index of ILCS chapters or acts.
- * 
+ * @param ulChildren children of a UL element that may contain a list of chapters or acts
+ * @param titleString string constant for formatting the title of a chapter or act
+ * @param urlString string constant for formatting the url to the page containing a chapter or act 
+ * @param topicString string constant for formatting the topic of a chapter or subtopic of an act.
+ * @param nlChar string constant for formatting a newline character
+ * @returns a formatted string representing a list index of ILCS chapters or acts.
  */
-export function getILCSIndexString(ulChildren, titleString, urlString, topicString, nlChar) {
+function getILCSIndexString(
+    ulChildren: Element[],
+    titleString: string,
+    urlString: string,
+    topicString: string,
+    nlChar: string):
+    string {
+
     let chapterIndexString  = '';
     let currentTopic = '';
     for(const ulChild of ulChildren) {
@@ -44,13 +50,16 @@ export function getILCSIndexString(ulChildren, titleString, urlString, topicStri
  * querySelectorAll call on 'td p *' selector on the appropriate page, and returns a string representing the entire text of an act.
  * Each section of the act is separated by the delimiter token.
  * 
- * @param {Element[]} pChildren the children of a P element that may contain a table of acts
- * @param {String} tokenString a string constant used as a delimiter token to separate acts 
- * @returns {String} actText - The formatted string representing the entire text of an act, with sections separated by tokenString
+ * @param pChildren the children of a P element that may contain a table of acts
+ * @param tokenString a string constant used as a delimiter token to separate acts 
+ * @returns a formatted string representing the entire text of an act, with sections separated by tokenString
  */
-export function getILCSAct(pChildren, tokenString) {
-    let actText = '';
+function getILCSAct(
+    pChildren: Element[],
+    tokenString: string):
+    string {
 
+    let actText = '';
     for (const pChild of pChildren) {
         if (pChild.tagName === 'TABLE') {
             actText += pChild.innerText + tokenString;
@@ -65,11 +74,15 @@ export function getILCSAct(pChildren, tokenString) {
  * the url of the 'View Entire Act' link, if it exists. It expects the result of a querySelectorAll call on 'td p a' selector on 
  * the appropriate page.
  * 
- * @param {Element[]} aNodes - an array of anchor elements that may contain a link to the entire act
- * @param {String} viewEntireAct - a string constant used to identify the link to the entire act 
- * @returns {String} href - the href attribute of the link to the entire act, or an empty string if the link is not found
+ * @param aNodes - an array of anchor elements that may contain a link to the entire act
+ * @param viewEntireAct - a string constant used to identify the link to the entire act 
+ * @returns a string with the href attribute found in the link to the entire act, if it exists, or an empty string if it doesn't
  */
-export function hasEntireAct(aNodes, viewEntireAct) {
+function hasEntireAct(
+    aNodes: Element[],
+    viewEntireAct: string):
+    string {
+        
     let href = '';
     for (const aNode of aNodes) {
         if (aNode.innerText.toLowerCase().includes(viewEntireAct)) {
@@ -79,3 +92,9 @@ export function hasEntireAct(aNodes, viewEntireAct) {
     }
     return href;
 }
+
+export { 
+    getILCSAct,
+    getILCSIndexString,
+    hasEntireAct
+ }
